e29985a51f68a0b1b18ff51dafb9e8d84768b208
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-04-18 16:22:59 +0300

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,11 +1,4 @@
 Python code:
 
 “”””
-import serial #Serial imported for Serial communication
-while 1:
-    incoming = str (ArduinoSerial.readline()) #read the serial data and print it as line
-    print incoming
-    
-    if 'Play/Pause' in incoming:
-        pyautogui.typewrite(['space'], 0.2)
 “”””
